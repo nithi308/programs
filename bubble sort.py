06fb7af37716fd4a3f83c1e986721a51361ef873
Nithish kumar <nithishkumarravi308@gmail.com>
2022-09-22 20:51:31 +0530

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,11 +1,7 @@
 def sort(arr):
-    n = len(arr)
-  
+    n = len(arr)  
     for i in range(n-1):
-
         for j in range(0, n-i-1):
-  
-            
             if arr[j] > arr[j + 1] :
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 def printlist(l1):
